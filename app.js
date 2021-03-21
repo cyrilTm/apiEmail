328b9f2f10ee92fb4cbf3c580973543179d4e286
cyrilTm <thomas_cyril@rocketmail.com>
2021-03-21 22:19:32 +0100

    cors

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+var cors = require('cors');
 
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+/*app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});*/
+  
 app.route('/')
     .get((req, res) => res.send('<h1>API Email</h1><p>API to send email</p>'));
 
